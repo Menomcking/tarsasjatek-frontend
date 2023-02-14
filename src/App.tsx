abc7b5b94fb92f5cd229e41adc7a9ec699d227e0
Menomcking <patrik8870@gmail.com>
2023-02-14 12:06:32 +0100

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+interface State {
+  tarsasjatekok: Tarsasjatek[];
+  tarsasjatekNev: string;
+  tarsasjatekKiadasiDatum: string;
+}
+interface Tarsasjatek{
+  id: number;
+  nev: string;
+  kiadasiDatum: Date
+}
+interface TarsasjatekListResponse{
+  tarsasjatekok: Tarsasjatek[];
+}
+
+class App extends Component <{}, State>{
+
+  constructor(props: {}){
+    super(props);
+
+    this.state = {
+      tarsasjatekok: [],
+      tarsasjatekNev: "",
+      tarsasjatekKiadasiDatum: ""
+    }
+  }
+  async loadTarsasjatekok(){
+    let response = await fetch('http://localhost:3001/tarsasjatek');
+    let data = await response.json() as Tarsasjatek[];
+    this.setState({tarsasjatekok: data})
+  }
+  componentDidMount(): void {
+    this.loadTarsasjatekok();
+  }
+  handleFill = async () => {
+    const {tarsasjatekNev, tarsasjatekKiadasiDatum} = this.state;
+    if(tarsasjatekNev.trim() == '' || tarsasjatekKiadasiDatum.trim() == ''){
+      return;
+    }
+    const data = {
+      nev: tarsasjatekNev,
+      kiadasiDatum: tarsasjatekKiadasiDatum,
+    };
+    let response = await fetch('http://localhost:3001/tarsasjatek', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    });
+    this.setState({
+      tarsasjatekNev: '',
+      tarsasjatekKiadasiDatum: '',
+    })
+    await this.loadTarsasjatekok();
+  }
+  async handleDelete(id:number){
+    await fetch('http://localhost:3001/tarsasjatek/'+ id, {
+      method: 'DELETE',
+    })
+    await this.loadTarsasjatekok();
+  }
+  render() {
+    const { tarsasjatekNev, tarsasjatekKiadasiDatum} = this.state;
+
+    return <div>
+      <div id='reg'>
+      <h2>Új Társasjáték</h2>
+      <p>Társasjáték neve: <input type="text" value={tarsasjatekNev} onChange={e => this.setState({tarsasjatekNev: e.currentTarget.value})}></input></p>
+      <p>Társasjáték kiadási dátuma: <input type="date" value={tarsasjatekKiadasiDatum} onChange={e => this.setState({tarsasjatekKiadasiDatum: e.currentTarget.value})}></input></p>
+      <p><button onClick={this.handleFill}>Adatfelvétel</button></p>
+      </div>
+      <h2>Társasjátékok:</h2>
+      <ul>
+      {this.state.tarsasjatekok.map(tarsasjatek =>  <li>{tarsasjatek.nev} <button onClick={() => this.handleDelete(tarsasjatek.id)}>Törlés</button></li>)}
+      </ul>
     </div>
-  );
+  }
+
 }
 
 export default App;
